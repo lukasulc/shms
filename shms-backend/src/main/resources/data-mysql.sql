1955d1ec9eae22d231537b38acd729e9836a924e
Luka Šulc <lukasulc0@gmail.com>
2024-12-10 00:56:08 +0100

    Add initial database entities

--- shms-backend/src/main/resources/data-mysql.sql
+++ shms-backend/src/main/resources/data-mysql.sql
@@ -4,7 +4,11 @@ VALUES
   (1000, "Bedroom light"),
   (1001, "Living room light"),
   (1002, "Bedroom thermostat"),
-  (1003, "Bathroom thermostat");
+  (1003, "Bathroom thermostat"),
+  (1004, "Bedroom blinds"),
+  (1005, "Living room blinds"),
+  (1006, "Porch camera"),
+  (1007, "Hallway camera");
 
 INSERT INTO
   light_device (id, state, brightness)
@@ -17,3 +21,15 @@ INSERT INTO
 VALUES
   (1002, 23.5, "OFF"),
   (1003, 24.5, "COOLING");
+
+INSERT INTO
+  camera_device (id, state, recording)
+VALUES
+  (1006, true, false),
+  (1007, true, false);
+
+INSERT INTO
+  blinds_device (id, state, position)
+VALUES
+  (1004, false, 0),
+  (1005, false, 100);
