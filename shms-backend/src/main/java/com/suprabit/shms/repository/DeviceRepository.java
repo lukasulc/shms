9f3eb000a0b3c531eb3405c2605b4143e19e9047
Luka Šulc <lukasulc0@gmail.com>
2024-12-09 15:20:12 +0100

    Remove @NoRepositoryBean from DeviceRepository

--- shms-backend/src/main/java/com/suprabit/shms/repository/DeviceRepository.java
+++ shms-backend/src/main/java/com/suprabit/shms/repository/DeviceRepository.java
@@ -4,11 +4,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.NoRepositoryBean;
 
 import com.suprabit.shms.model.Device;
 
-@NoRepositoryBean
 public interface DeviceRepository<T extends Device> extends CrudRepository<T, Long> /* , JpaRepository<T, Long> */ {
 
     @Query("select u from #{#entityName} as u where u.name = ?1 ")
